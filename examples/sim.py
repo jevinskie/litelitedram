68d8a45f573043ce86b0cc29aac281a12d897fc3
Jevin Sweval <jevinsweval@gmail.com>
2022-07-12 21:13:17 -0700

    borked

--- examples/sim.py
+++ examples/sim.py
@@ -7,13 +7,18 @@ import argparse
 from pathlib import Path
 
 from liteeth.phy.model import LiteEthPHYModel
+from litescope import LiteScopeAnalyzer
 from litex.build.generic_platform import *
 from litex.build.sim import SimPlatform, sim_build_argdict, sim_build_args
 from litex.build.sim.config import SimConfig
 from litex.gen.fhdl.namer import escape_identifier_name
+from litex.soc.cores import uart
 from litex.soc.integration.builder import *
 from litex.soc.integration.soc_core import *
 from migen import *
+
+from litelitedram.ddr3 import SlowDDR3
+from litelitedram.ddr3_model import DDR3Model, DDR3PhyInterface
 
 # IOs ----------------------------------------------------------------------------------------------
 
@@ -30,6 +35,17 @@ _io = [
     ),
     (
         "eth",
+        0,
+        Subsignal("source_valid", Pins(1)),
+        Subsignal("source_ready", Pins(1)),
+        Subsignal("source_data", Pins(8)),
+        Subsignal("sink_valid", Pins(1)),
+        Subsignal("sink_ready", Pins(1)),
+        Subsignal("sink_data", Pins(8)),
+    ),
+    # Serial.
+    (
+        "serial",
         0,
         Subsignal("source_valid", Pins(1)),
         Subsignal("source_ready", Pins(1)),
@@ -59,7 +75,15 @@ class Platform(SimPlatform):
 
 
 class SimSoC(SoCCore):
-    def __init__(self, sim_toolchain, sys_clk_freq=None, **kwargs):
+    def __init__(
+        self,
+        sim_toolchain,
+        sys_clk_freq=None,
+        with_analyzer=False,
+        with_etherbone=False,
+        with_uartbone=False,
+        **kwargs,
+    ):
         platform = Platform(sim_toolchain)
         sys_clk_freq = int(sys_clk_freq)
 
@@ -76,8 +100,51 @@ class SimSoC(SoCCore):
         self.submodules.crg = CRG(platform.request("sys_clk"))
 
         # Etherbone --------------------------------------------------------------------------------
-        self.submodules.ethphy = LiteEthPHYModel(self.platform.request("eth"))
-        self.add_etherbone(phy=self.ethphy, ip_address="192.168.42.50")
+        if with_etherbone:
+            self.submodules.ethphy = LiteEthPHYModel(self.platform.request("eth"))
+            self.add_etherbone(phy=self.ethphy, ip_address="192.168.42.50")
+
+        # UARTbone ---------------------------------------------------------------------------------
+        if with_uartbone:
+            self.submodules.uartbone_phy = uart.RS232PHYModel(platform.request("serial"))
+            self.submodules.uartbone = uart.UARTBone(phy=self.uartbone_phy, clk_freq=sys_clk_freq)
+            self.bus.add_master(name="uartbone", master=self.uartbone.wishbone)
+
+        # Slow DDR3 --------------------------------------------------------------------------------
+        # ddr3_pads = DDR3PhyInterface()
+        # self.submodules.ddr = SlowDDR3(self.platform, ddr3_pads, sys_clk_freq, debug=True)
+        # dram_base = 0x2000_0000
+        # self.add_memory_region("dram", dram_base, self.ddr.bitsize // 8, type="")
+        # self.bus.add_slave("dram", self.ddr.bus)
+        # self.submodules.ddr_model = DDR3Model(self.platform, ddr3_pads)
+        # self.register_mem("dram", dram_base, self.ddr.bus, size=self.ddr.bitsize // 8)
+
+        # scope ------------------------------------------------------------------------------------
+        if with_analyzer:
+            analyzer_signals = [
+                ddr3_pads.a,
+                ddr3_pads.ba,
+                ddr3_pads.cas_n,
+                ddr3_pads.ras_n,
+                ddr3_pads.we_n,
+                ddr3_pads.cs_n,
+                ddr3_pads.dm,
+                ddr3_pads.dq,
+                # ddr3_pads.dqs_p,
+                self.ddr.init_state,
+                self.ddr.work_state,
+                self.ddr.refresh_cnt,
+                self.ddr.refresh_issued,
+                self.ddr.bus,
+                self.ddr.sysio,
+            ]
+            self.submodules.analyzer = LiteScopeAnalyzer(
+                analyzer_signals,
+                depth=4096,
+                clock_domain="sys",
+                rle_nbits_min=15,
+                csr_csv="analyzer.csv",
+            )
 
 
 #
@@ -89,6 +156,9 @@ def sim_args(parser):
     soc_core_args(parser)
     sim_build_args(parser)
     parser.add_argument("--debug-soc-gen", action="store_true", help="Don't run simulation")
+    parser.add_argument("--with-analyzer", action="store_true", help="Use litescope")
+    parser.add_argument("--with-etherbone", action="store_true", help="Use Etherbone")
+    parser.add_argument("--with-uartbone", action="store_true", help="Use UARTbone")
 
 
 def main():
@@ -100,7 +170,10 @@ def main():
 
     sim_config = SimConfig()
     sim_config.add_clocker("sys_clk", freq_hz=sys_clk_freq)
-    sim_config.add_module("ethernet", "eth", args={"interface": "tap0", "ip": "192.168.42.100"})
+    if args.with_etherbone:
+        sim_config.add_module("ethernet", "eth", args={"interface": "tap0", "ip": "192.168.42.100"})
+    if args.with_uartbone:
+        sim_config.add_module("serial2tcp", "serial", args={"port": 2430})
 
     soc_kwargs = soc_core_argdict(args)
     builder_kwargs = builder_argdict(args)
@@ -111,7 +184,13 @@ def main():
     soc_kwargs["with_uart"] = False
     soc_kwargs["ident_version"] = True
 
-    soc = SimSoC(sim_toolchain=args.sim_toolchain, **soc_kwargs)
+    soc = SimSoC(
+        sim_toolchain=args.sim_toolchain,
+        with_analyzer=args.with_analyzer,
+        with_etherbone=args.with_etherbone,
+        with_uartbone=args.with_uartbone,
+        **soc_kwargs,
+    )
 
     builder_kwargs["csr_csv"] = "csr.csv"
     builder_kwargs["output_dir"] = soc.platform.output_dir
--- litelitedram/ddr3_model.py
+++ litelitedram/ddr3_model.py
@@ -1,49 +1,88 @@
 import os
 import subprocess
-from enum import IntEnum
-from math import ceil, log2
 
-from litex.soc.interconnect import wishbone
 from migen import *
 from migen.genlib.record import DIR_M_TO_S, DIR_NONE
+
+
+class DDR3PhyInterface(Record):
+    def __init__(self, row_width=14, dq_width=16):
+        super().__init__(
+            [
+                ("a", row_width, DIR_M_TO_S),
+                ("ba", 3, DIR_M_TO_S),
+                ("ras_n", 1, DIR_M_TO_S),
+                ("cas_n", 1, DIR_M_TO_S),
+                ("we_n", 1, DIR_M_TO_S),
+                ("dm", dq_width // 8, DIR_M_TO_S),
+                ("dq", dq_width, DIR_NONE),
+                ("dqs_p", dq_width // 8, DIR_NONE),
+                ("dqs_n", dq_width // 8, DIR_NONE),
+                ("clk_p", 1, DIR_M_TO_S),
+                ("clk_n", 1, DIR_M_TO_S),
+                ("cs_n", 1, DIR_M_TO_S),
+                ("cke", 1, DIR_M_TO_S),
+                ("odt", 1, DIR_M_TO_S),
+                ("reset_n", 1, DIR_M_TO_S),
+            ]
+        )
 
 
 class DDR3ModelInterface(Record):
     def __init__(self, row_width, dq_width):
         super().__init__(
             [
-                ("address", row_width, DIR_M_TO_S),
-                ("bank", 3, DIR_M_TO_S),
-                ("cs", 1, DIR_M_TO_S),
-                ("cas", 1, DIR_M_TO_S),
-                ("ras", 1, DIR_M_TO_S),
-                ("we", 1, DIR_M_TO_S),
-                ("clk_p", 1, DIR_M_TO_S),
+                ("addr", row_width, DIR_M_TO_S),
+                ("ba", 3, DIR_M_TO_S),
+                ("ras_n", 1, DIR_M_TO_S),
+                ("cas_n", 1, DIR_M_TO_S),
+                ("we_n", 1, DIR_M_TO_S),
+                ("dm_tdqs", dq_width // 8, DIR_M_TO_S),
+                ("dq", dq_width, DIR_NONE),
+                ("dqs", dq_width // 8, DIR_NONE),
+                ("dqs_n", dq_width // 8, DIR_NONE),
+                ("ck", 1, DIR_M_TO_S),
+                ("ck_n", 1, DIR_M_TO_S),
+                ("cs_n", 1, DIR_M_TO_S),
                 ("cke", 1, DIR_M_TO_S),
                 ("odt", 1, DIR_M_TO_S),
                 ("rst_n", 1, DIR_M_TO_S),
-                ("cs", 1, DIR_M_TO_S),
-                ("dm", dq_width // 8, DIR_M_TO_S),
-                ("dq", dq_width, DIR_NONE),
-                ("dqs_p", dq_width // 8, DIR_NONE),
-                ("dqs_n", dq_width // 8, DIR_NONE),
             ]
         )
 
 
 class DDR3Model(Module):
-    def __init__(self, platform):
+    def __init__(self, platform, pads):
         self.platform = platform
-        ports = {}
+        ports = dict(
+            o_addr=pads.a,
+            o_ba=pads.ba,
+            o_ras_n=pads.ras_n,
+            o_cas_n=pads.cas_n,
+            o_we_n=pads.we_n,
+            io_dm_tdqs=pads.dm,
+            io_dq=pads.dq,
+            io_dqs=pads.dqs_p,
+            io_dqs_n=pads.dqs_n,
+            o_ck=pads.clk_p,
+            o_ck_n=pads.clk_n,
+            o_cs_n=pads.cs_n,
+            o_cke=pads.cke,
+            o_odt=pads.odt,
+            o_rst_n=pads.reset_n,
+        )
         self.specials += Instance("ddr3", name="ddr3_model", **ports)
 
     def do_finalize(self):
-        model_dir = os.path.join(self.platform.output_dir, "gateware")
-        verilog_filename = os.path.join(verilog_dir, "slowDDR3.v")
         slowddr3_dir = os.path.normpath(
             os.path.join(os.path.dirname(__file__), "..", "3rdparty", "General-Slow-DDR3-Interface")
         )
-        ddr3_model_file = os.path.join(slowddr3_dir, "model", "ddr3.v")
-        subprocess.check_call(["make" "model/ddr3.v"], cwd=slowddr3_dir)
+        ddr3_model_dir = os.path.join(slowddr3_dir, "model")
+        ddr3_model_file = os.path.join(ddr3_model_dir, "ddr3.v")
+        subprocess.check_call(["make", "model/ddr3.v"], cwd=slowddr3_dir)
+        self.platform.add_verilog_include_path(ddr3_model_dir)
+        for d in ("sg25", "x16", "den2048Mb"):
+            self.platform.add_compiler_definition(d)
         self.platform.add_source(ddr3_model_file)
-        self.platform
+        # incdir
+        # compile defs
